50b68768bdf890812ed53cb52887c2abcc0ad0fb
manue1 <manuel.bergler@gmail.com>
2016-09-23 11:50:51 -0500

    Serve from /widget.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
-# FROM mhart/alpine-node:base
+FROM mhart/alpine-node:base
 # FROM mhart/alpine-node:base-0.10
-FROM mhart/alpine-node
+# FROM mhart/alpine-node
 
 WORKDIR /src
 ADD . .
@@ -9,7 +9,7 @@ ADD . .
 # RUN apk add --no-cache make gcc g++ python
 
 # If you need npm, don't use a base tag
-RUN npm install
+# RUN npm install
 
 EXPOSE 3000
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/release/dist'));
+app.use('/widget', express.static(__dirname + '/release/dist'));
 
 app.use(function(err, req, res, next) {
   console.error(err.stack);
@@ -10,28 +10,28 @@ app.use(function(err, req, res, next) {
 app.listen(process.env.PORT || 4000, function() {
   console.log('Express server listening.');
 });
-// 
-// var execFile = require('child_process').execFile;
-// var githubhook = require('githubhook');
-// var github = githubhook({
-//     host: '0.0.0.0',
-//     port: 3240,
-//     path: '/githook',
-//     secret: 'zfj21rKWOrvJ6jTCI5rW',
-//     logger: console,
-// });
-//
-// github.listen();
-//
-// github.on('push:cc-calculator-widget:refs/heads/master', function (data) {
-//     // Exec a shell script
-//     var execOptions = {
-//         maxBuffer: 1024 * 1024 // Increase max buffer to 1mb
-//     };
-//     execFile(__dirname + 'deploy.sh', execOptions, function(error, stdout, stderr) {
-//         if( error )
-//         {
-//             console.log(error)
-//         }
-//     });
-// });
+
+var execFile = require('child_process').execFile;
+var githubhook = require('githubhook');
+var github = githubhook({
+    host: '0.0.0.0',
+    port: 3240,
+    path: '/githook',
+    secret: 'zfj21rKWOrvJ6jTCI5rW',
+    logger: console,
+});
+
+github.listen();
+
+github.on('push:cc-calculator-widget:refs/heads/master', function (data) {
+    // Exec a shell script
+    var execOptions = {
+        maxBuffer: 1024 * 1024 // Increase max buffer to 1mb
+    };
+    execFile(__dirname + 'deploy.sh', execOptions, function(error, stdout, stderr) {
+        if( error )
+        {
+            console.log(error)
+        }
+    });
+});
