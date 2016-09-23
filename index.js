3600e28bc43eec6b6ca6708c5df7724c39a23a4a
manue1 <manuel.bergler@gmail.com>
2016-09-23 12:27:40 -0500

    Replace github webhook package.

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
 var express = require('express');
 var app = express();
 
+var createHandler = require('github-webhook-handler')
+var handler = createHandler({ path: '/widget/githook', secret: 'zfj21rKWOrvJ6jTCI5rW' })
 var execFile = require('child_process').execFile;
-var githubhook = require('githubhook');
-var github = githubhook({
-    host: '0.0.0.0',
-    port: 4000,
-    path: '/widget/githook',
-    secret: 'zfj21rKWOrvJ6jTCI5rW',
-    logger: console,
-});
 
 app.use('/widget', express.static(__dirname + '/release/dist'));
 
 app.get('/widget/githook', function(req, res) {
-  github.on('push:cc-calculator-widget:refs/heads/master', function (data) {
-      // Exec a shell script
-      var execOptions = {
-          maxBuffer: 1024 * 1024 // Increase max buffer to 1mb
-      };
-      execFile(__dirname + 'deploy.sh', execOptions, function(error, stdout, stderr) {
-          if( error )
-          {
-              console.log(error)
-          }
-      });
+  handler(req, res, function (err) {
+      res.statusCode = 404
+      res.end('no such location')
   });
 });
+
+
+
+handler.on('error', function (err) {
+  console.error('Error:', err.message)
+})
+
+handler.on('push', function (event) {
+  console.log('Received a push event for %s to %s',
+    event.payload.repository.name,
+    event.payload.ref)
+    // Exec a shell script
+    var execOptions = {
+        maxBuffer: 1024 * 1024 // Increase max buffer to 1mb
+    };
+    execFile(__dirname + 'deploy.sh', execOptions, function(error, stdout, stderr) {
+        if( error )
+        {
+            console.log(error)
+        }
+    });
+})
 
 app.listen(process.env.PORT || 4000, function() {
   console.log('Express server listening.');
